dc4240ec0b9554bbe93973d1f8d088b9652fcedb
PotcFdk <PotcFdk@gmx.net>
2020-05-24 15:52:14 +0200

    Bump version number to 0.1.0 to ensure triggering the update from old installations.

--- SteamLinkFilterBypass/SteamLinkFilterBypass.meta.js
+++ SteamLinkFilterBypass/SteamLinkFilterBypass.meta.js
@@ -5,7 +5,7 @@
 // @description Skips the Steam LinkFilter.
 // @include     https://steamcommunity.com/linkfilter/*
 // @include     http://steamcommunity.com/linkfilter/*
-// @version     0.0.3
+// @version     0.1.0
 // @grant       none
 // @downloadURL https://raw.githubusercontent.com/PotcFdk/userscripts/master/SteamLinkFilterBypass/SteamLinkFilterBypass.user.js
 // @updateURL   https://raw.githubusercontent.com/PotcFdk/userscripts/master/SteamLinkFilterBypass/SteamLinkFilterBypass.meta.js
