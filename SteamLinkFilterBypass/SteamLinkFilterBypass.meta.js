eb18c8d9884ede83b80a8e81949d436b5c6c8517
PotcFdk <PotcFdk@gmx.net>
2020-05-24 16:19:44 +0200

    Add @icon to SteamLinkFilterBypass.

--- SteamLinkFilterBypass/SteamLinkFilterBypass.meta.js
+++ SteamLinkFilterBypass/SteamLinkFilterBypass.meta.js
@@ -7,6 +7,7 @@
 // @include     http://steamcommunity.com/linkfilter/*
 // @version     0.1.0
 // @grant       none
+// @icon        https://steamcommunity-a.akamaihd.net/public/images//linkfilter/mascot_sir_roderick_warning.png
 // @homepageURL https://github.com/PotcFdk/userscripts/tree/master/SteamLinkFilterBypass
 // @supportURL  https://github.com/PotcFdk/userscripts/issues
 // @downloadURL https://raw.githubusercontent.com/PotcFdk/userscripts/master/SteamLinkFilterBypass/SteamLinkFilterBypass.user.js
